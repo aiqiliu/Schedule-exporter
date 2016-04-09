a6c32ec69e1ef8785960b9bc799f0d23fa4d7b21
aiqi <liuaiqi1997@hotmail.com>
2016-04-09 17:13:38 -0500

    selenium login to caesar

--- scraper.py
+++ scraper.py
@@ -1,8 +1,42 @@
-from bs4 import BeautifulSoup
-import urllib 
+#!/usr/bin/env ruby
+#scrape class schedule from caesar
+import os
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
-url = urllib.urlopen('https://ses.ent.northwestern.edu/psp/caesar/EMPLOYEE/HRMS/c/SA_LEARNER_SERVICES_2.SSR_SSENRL_CART.GBL?pslnkid=NW_SS_SA_SHOPPING_CART&FolderPath=PORTAL_ROOT_OBJECT.NW_SS_ENROLLMENT.NW_SS_SA_SHOPPING_CART&IsFolder=false&IgnoreParamTempl=FolderPath%2cIsFolder').read()
-soup = BeautifulSoup(url, "html.parser")
-print type(soup)
-print soup.prettify()[0:1000]
+#login to caesar
+print("Please type in your netID: ")
+netid = raw_input()
+print("Please type in your caesar password")
+pwd = raw_input()
+
+chromedriver = "/Users/john/Downloads/chromedriver"
+os.environ["webdriver.chrome.driver"] = chromedriver
+driver = webdriver.Chrome(chromedriver)
+
+driver.get('https://ses.ent.northwestern.edu/psp/s9prod/?cmd=login')
+driver.find_element_by_id('userid').send_keys(netid)
+driver.find_element_by_id("pwd").send_keys(pwd)
+
+driver.find_element_by_name("Submit").click()
+
+#get to class selection page
+driver = driver.get('https://ses.ent.northwestern.edu/psp/caesar_5/EMPLOYEE/HRMS/c/SA_LEARNER_SERVICES.SSR_SSENRL_APPT.GBL?Page=SSR_SSENRL_APPT&Action=A&EMPLID=2870114&TargetFrameName=None')
+# driver.find_element_by_name("SSR_DUMMY_RECV1$sels$0").click()
+# driver.find_element_by_name("DERIVED_SSS_SCT_SSR_PB_GO").click()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 

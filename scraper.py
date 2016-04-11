c8b58ee101fd42e98c677fd5dc786bb86b8ede6d
aiqi <liuaiqi1997@hotmail.com>
2016-04-10 23:32:29 -0500

    for the sake of keeping my streak of commits

--- scraper.py
+++ scraper.py
@@ -7,7 +7,7 @@ from selenium.webdriver.common.keys import Keys
 #login to caesar
 print("Please type in your netID: ")
 netid = raw_input()
-print("Please type in your caesar password")
+print("Please type in your Caesar password")
 pwd = raw_input()
 
 chromedriver = "/Users/john/Downloads/chromedriver"
@@ -22,6 +22,7 @@ driver.find_element_by_name("Submit").click()
 
 #get to class selection page
 driver = driver.get('https://ses.ent.northwestern.edu/psp/caesar_5/EMPLOYEE/HRMS/c/SA_LEARNER_SERVICES.SSR_SSENRL_APPT.GBL?Page=SSR_SSENRL_APPT&Action=A&EMPLID=2870114&TargetFrameName=None')
+print(driver)
 # driver.find_element_by_name("SSR_DUMMY_RECV1$sels$0").click()
 # driver.find_element_by_name("DERIVED_SSS_SCT_SSR_PB_GO").click()
 
